f5e0869b3f5ac3ea04921f46f0715c7ab90518c7
Anna Podobrii <anna@nearmeentertainment.com>
2023-05-16 15:56:31 +0300

    Initial Commit

---
+++ TouchpadHandler/TouchpadHandlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchpadHandlerApp.swift
+//  TouchpadHandler
+//
+//  Created by Anna Podobrii on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchpadHandlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
